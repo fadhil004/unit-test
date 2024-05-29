44bc5e55dd5e674f7f94b1ccc0b7cb58ab0f07f7
fadhil004 <m.fadhil238@gmail.com>
2024-05-29 11:53:58 +0700

    assertion

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,41 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHelloWorldAssert(t *testing.T)  {
+	result := HelloWorld("Fadhil")
+	assert.Equal(t, "Hello Fadhil", result, "Result must be 'Hello Fadhil'")
+	fmt.Println("TestHelloWorld with Assert done")
+}
+
+func TestHelloWorldRequire(t *testing.T)  {
+	result := HelloWorld("Fadhil")
+	require.Equal(t, "Hello Fadhil", result, "Result must be 'Hello Fadhil'")
+	fmt.Println("TestHelloWorld with Assert done")
+}
 
 func TestHelloWorld(t *testing.T)  {
 	result := HelloWorld("Fadhil")
 	if result != "Hello Fadhil" {
 		//unit test failed
-		panic("Result is not 'Hello Fadhil'")
+		t.Error("Result must be 'Hello Fadhil'")
 	}
+
+	fmt.Println("TestHelloWorld Done")
+}
+
+func TestHelloWorldFaraz(t *testing.T)  {
+	result := HelloWorld("Faraz")
+	if result != "Hello Faraz" {
+		//unit test failed
+		t.Fatal("Result must be 'Hello Faraz'")
+	}
+
+	fmt.Println("TestHelloWorldFaraz Done")
 }

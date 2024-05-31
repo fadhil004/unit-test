b318f6a2ad612299375f891707581c0da9bcdfa9
fadhil004 <m.fadhil238@gmail.com>
2024-05-31 22:34:33 +0700

    skip test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,21 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestSkip(t *testing.T)  {
+	if runtime.GOOS == "windows" {
+		t.Skip("Can't run on windows")
+	}
+
+	result := HelloWorld("Fadhil")
+	require.Equal(t, "Hello Fadhil", result, "Result must be 'Hello Fadhil'")
+}
 
 func TestHelloWorldAssert(t *testing.T)  {
 	result := HelloWorld("Fadhil")
